09f808fac16ba83fa6232f21bffc1719ca6ab19d
John Lee <johnleenimh@gmail.com>
2017-03-15 16:28:25 -0400

    just download a csv

--- metasearch_analysis.py
+++ metasearch_analysis.py
@@ -1,9 +1,3 @@
 # coding: utf-8
-from pathlib import Path
-metasearch_dir = Path('data_not_in_repo/metasearch/')
-data_exists = metasearch_dir.exists()
-in_repro_course = Path.cwd().name == 'repro_course'
-if data_exists and in_repro_course:
-    print('data has been downloaded')
-else:
-    assert(False)
+import pandas as pd
+df_pheno = pd.read_csv('https://raw.githubusercontent.com/OpenNeuroLab/metasearch/master/docs/data/phenotype_mri.csv')
